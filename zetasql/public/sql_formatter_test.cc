eb0c22168da59a2d6240e1019f586c671bc24d5e
Matts966 <Matts966@users.noreply.github.com>
2020-10-05 00:45:29 +0900

    Try to resolve the HasSubstr's weird behavior

--- zetasql/public/sql_formatter_test.cc
+++ zetasql/public/sql_formatter_test.cc
@@ -84,14 +84,14 @@ TEST(SqlFormatterTest, InvalidSingleStatement) {
       FormatSql("select f1 as a from T having a > 5 having a > 5; # foo",
                 &formatted_sql),
       StatusIs(_,
-               HasSubstr("INVALID_ARGUMENT: Syntax error: Expected end of input but got keyword HAVING")));
+               HasSubstr("INVALID_ARGUMENT: Syntax error: Expected end of input but got keyword HAVING [at 1:36]")));
   EXPECT_EQ("select f1 as a from T having a > 5 having a > 5; # foo",
             formatted_sql);
 
   // Empty statement.
   EXPECT_THAT(
       FormatSql(";", &formatted_sql),
-      StatusIs(_, HasSubstr("INVALID_ARGUMENT: Syntax error: Expected end of input but got")));
+      StatusIs(_, HasSubstr("INVALID_ARGUMENT: Syntax error: Expected end of input but got \";\" [at 1:1]")));
   EXPECT_EQ(";", formatted_sql);
 
   // Semicolon in string.
@@ -142,7 +142,7 @@ TEST(SqlFormatterTest, InvalidMultipleStatements) {
           &formatted_sql),
       StatusIs(
           _,
-          HasSubstr("INVALID_ARGUMENT: Syntax error: Unexpected")));
+          HasSubstr("INVALID_ARGUMENT: Syntax error: Unexpected \".\" [at 1:10]")));
   EXPECT_EQ(" drop foo.bar;  define table t1 (a=1,b=\"a\",c=1.4,d=true) ;\n"
             " select sum(f1) as a from T having a > 5 having a > 5;select 1",
             formatted_sql);
@@ -150,7 +150,7 @@ TEST(SqlFormatterTest, InvalidMultipleStatements) {
   // The second statement is an invalid empty statement.
   EXPECT_THAT(
       FormatSql("select 1;  ;", &formatted_sql),
-      StatusIs(_, HasSubstr("INVALID_ARGUMENT: Syntax error: Expected end of input but got")));
+      StatusIs(_, HasSubstr("INVALID_ARGUMENT: Syntax error: Expected end of input but got \";\" [at 1:12]")));
   EXPECT_EQ("select 1;  ;",
             formatted_sql);
 
